5008d7509adfa165e73775dee0254bea806a19f6
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2023-01-30 18:28:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -53,20 +53,20 @@ formInputEl.addEventListener("submit", (e) => {
 })
 
 /*function reuble*****/
-function renderSubMenu(id,data){
+function renderSubMenu(id, data) {
     let temp = document.getElementById(id)
-function TempFunc(){
-   return data.map(el =>{
-        let list = ""; 
-        list = el.data.map(element => `<p>${element}</p>`).join(" ")    
-       return `
+    function TempFunc() {
+        return data.map(el => {
+            let list = "";
+            list = el.data.map(element => `<p>${element}</p>`).join(" ")
+            return `
         <div class="column">
             <h4>${el.heading}</h4>
             ${list}
         </div>
        `
-    }).join("")
-}
+        }).join("")
+    }
     temp.innerHTML = TempFunc()
 }
 
@@ -76,33 +76,116 @@ function TempFunc(){
 
 
 /****womenEthic */
-renderSubMenu("womenEthic",WomenEthnic)
+renderSubMenu("womenEthic", WomenEthnic)
 
 /****WomenWestern */
-renderSubMenu("womenWestern",WomenWestern)
+renderSubMenu("womenWestern", WomenWestern)
 
 //Men 
-renderSubMenu("men",Men)
+renderSubMenu("men", Men)
 
 /***kids */
-renderSubMenu("kids",Kids)
+renderSubMenu("kids", Kids)
 
 /**home % kitchen */
-renderSubMenu("HomeAndKitchen",HomeAndKitchen)
+renderSubMenu("HomeAndKitchen", HomeAndKitchen)
 
 /**beauty and health */
-renderSubMenu("beautyAndHealth",BeautyHealth)
+renderSubMenu("beautyAndHealth", BeautyHealth)
 
 // Jewellery & Accessories
-renderSubMenu("JewelleryAndAccessories",JewelleryAccessories)
+renderSubMenu("JewelleryAndAccessories", JewelleryAccessories)
 
 // Bags & Footwear
-renderSubMenu("BagsFootWarId",BagsFootwear)
+renderSubMenu("BagsFootWarId", BagsFootwear)
 
 // Electronics
-renderSubMenu("ElectronicsId",Electronics)
+renderSubMenu("ElectronicsId", Electronics)
 
 
+
+/**********product section***************/
+import ProductData from "./meesho/data.js"
+
+const category = [...new Set(ProductData.map(el => el.category))]
+console.log(category)
+
+
+let filterData = []
+
+document.addEventListener("click", (e) => {
+
+
+    const bluetoothEl = document.getElementById("bluetooth").checked
+    const ChainsEl = document.getElementById("chains").checked
+    const KurtasEl = document.getElementById("kurtas").checked
+    const AccessoriesEl = document.getElementById("accessories").checked
+    const sareesEl = document.getElementById("sarees").checked
+    const watchEl = document.getElementById("watch").checked
+
+    console.log(bluetoothEl)
+    filterData = ProductData.filter(el => (
+        bluetoothEl && el.category == "bluetooth Headset" ||
+        ChainsEl && el.category == "Men Chains" ||
+        KurtasEl && el.category == "Kurtas" ||
+        AccessoriesEl && el.category == "Mobile Accessories" ||
+        sareesEl && el.category == "sarees" ||
+        watchEl && el.category == "watch"
+    ))
+
+    renderProductData()
+
+
+})
+
+function renderProductData(){
+    let filterDataHTML = "";
+
+    if(filterData[0]){
+        filterData.forEach(el => {
+            filterDataHTML += `
+            <div class="productCard" onclick="ClickProduct(${el.id})">
+                <div class="product_image">
+                    <img src="./meesho/productImage/${el.img}"/>
+                </div>
+                <h3 class="product_name">${el.name}</h3>
+                <p class="product_price">
+                    <span>₹</span>
+                    <span>${el.price}</span>
+                </p>
+             </div>
+            `
+        })
+    }
+    else{
+        ProductData.forEach(el => {
+            filterDataHTML += `
+            <div class="productCard" onclick="ClickProduct(${el.id})">
+                <div class="product_image">
+                    <img src="./meesho/productImage/${el.img}"/>
+                </div>
+                <h3 class="product_name">${el.name}</h3>
+                <p class="product_price">
+                    <span>₹</span>
+                    <span>${el.price}</span>
+                </p>
+             </div>
+            `
+        }) 
+    }
+    
+    document.getElementById("product_category_displayId").innerHTML = filterDataHTML
+} 
+renderProductData()
+
+
+
+function ClickProduct(id){
+    localStorage.setItem("productId",JSON.stringify(id))
+    // window.location("./page/product.html")
+
+    alert("hii")
+}
 
 
 
